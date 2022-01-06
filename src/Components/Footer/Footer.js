bafe47043985bcf3bd14f916f2943ab759782d0d
MD-ABUSUFIAN <abusufian.programmer@gmail.com>
2022-01-06 21:56:03 +0600

    footer page modified

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css'
 
 const Footer = () => {
     return (
-        <div className="bg-secondary py-3">
+        <div className="bg-black py-2">
            <a href=""> <img className=" footer-icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/2021_Facebook_icon.svg/800px-2021_Facebook_icon.svg.png" alt="" /></a>
            <a href=""> <img className=" w-25 footer-icon" src="https://seeklogo.com/images/I/instagram-new-2016-logo-4773FE3F99-seeklogo.com.png" alt="" /></a>
            <a href=""> <img className=" w-25 footer-icon" src="https://cdn-icons-png.flaticon.com/512/174/174857.png" alt="" /></a>
